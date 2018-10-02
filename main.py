8517e983a383e91e5f405870742ef2ba6c287ea4
Dave Neary <dneary@gnome.org>
2018-10-02 19:37:24 +0000

    Initial commit for very simple FizzBuzz project

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+for i in range(1, 101):
+    str = ""
+
+    if (i % 3 == 0):
+        str += "Fizz"
+    if (i % 5 == 0):
+        str += "Buzz"
+    if (str == ""):
+        str = "%d" % i
+    print("%s" % str)
